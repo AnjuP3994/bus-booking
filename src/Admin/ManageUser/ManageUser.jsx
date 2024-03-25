16a7b989c5e92e5d2b9e2ecc480b64947ec4aad5
Sanjay G <sanjaysanju7560@gmail.com>
2024-03-25 16:41:29 +0530

    changes in ui for headers

--- src/Admin/ManageUser/ManageUser.jsx
+++ src/Admin/ManageUser/ManageUser.jsx
@@ -4,17 +4,14 @@ import Stack from '@mui/material/Stack';
 import { Table } from 'react-bootstrap';
 import bgImage  from "./assets/manage-user-bg.jpg"
 import profileImage  from "./assets/user_img.jpg"
-<<<<<<< HEAD
 import Swal from 'sweetalert2';
 
 import './mangeUser.css'
 import { deleteUser, getUser } from '../../Services/allAPIs';
 import { BASE_URL } from '../../Services/baseURL';
-=======
 import './mangeUser.css'
 import AdminHeader from '../Header/AdminHeader';
 
->>>>>>> f31845cf5e8ebc3e9b49a1569cd69d6fa89f48fa
 function ManageUser() {
     const [users,setUsers]=useState([])
 
@@ -81,12 +78,7 @@ function ManageUser() {
                 <h1 className='d-flex fw-bolder ms-5 ps-5 pt-5  '>Manage User</h1>
                 <div className='containter' style={{ height: '50vh', overflowY: 'auto'}}>
                     <div className='p-5 ms-5 me-5' >
-<<<<<<< HEAD
-
-                        <Table bordered hover style={{borderColor:'black',fontSize: '1.5rem'}} >
-=======
                         <Table bordered hover style={{borderColor:'black'}} >
->>>>>>> f31845cf5e8ebc3e9b49a1569cd69d6fa89f48fa
                             <thead>
                                 <tr style={{backgroundColor:'#0095a9'}}  className='table-info fw-bold fs-3'>
                                     <th>#</th>
@@ -99,7 +91,6 @@ function ManageUser() {
                                 </tr>
                             </thead>
                             <tbody>
-<<<<<<< HEAD
                                { users?.length>0?
                                users.map((item,index)=>(<tr className='fs-3'>
                                <td>{index+1}</td>
@@ -117,99 +108,6 @@ function ManageUser() {
                            ))
                                 : <p>nothing to show</p> }
                                 
-=======
-                                <tr className='fs-3'>
-                                    <td>1</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Mark </p>
-                                    </td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>01/01/2001</td>
-                                    <td>asdf 620555</td>
-                                    <td className=' text-center'><i className="fa-solid fa-trash "></i></td>
-                                </tr>
-                                <tr className='fs-3'>
-                                    <td>2</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Jacob </p>
-                                    </td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>02/02/2002</td>
-                                    <td>asdf 620555</td>
-                                    <td  className=' text-center'><i class="fa-solid fa-trash"></i></td>
-                                </tr>
-                                <tr className='fs-3'>
-                                    <td>3</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Jacob </p>
-                                    </td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>02/02/2002</td>
-                                    <td>asdf 620555</td>
-                                    <td className=' text-center'><i className="fa-solid fa-trash"></i></td>
-                                </tr>
-                                <tr className='fs-3'>
-                                    <td>4</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Jacob </p>
-                                    </td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>02/02/2002</td>
-                                    <td>asdf 620555</td>
-                                    <td className=' text-center'><i className="fa-solid fa-trash"></i></td>
-                                </tr>
-                                <tr className='fs-3'>
-                                    <td>5</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Jacob </p>
-                                    </td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>02/02/2002</td>
-                                    <td>asdf 620555</td>
-                                    <td className=' text-center'><i className="fa-solid fa-trash" ></i></td>
-                                </tr>
-                                <tr className='fs-3'>
-                                    <td>6</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Jacob </p>
-                                    </td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>02/02/2002</td>
-                                    <td>asdf 620555</td>
-                                    <td className=' text-center'><i className="fa-solid fa-trash"></i></td>
-                                </tr>
-                                <tr className='fs-3'>
-                                    <td>7</td>
-                                    <td  className='d-flex'>
-                                         <Stack direction="row" >
-                                         <Avatar  alt="Remy Sharp" src={profileImage} sx={{ width: 35, height: 35 }} /> </Stack>
-                                         <p className='ms-5 mt-2'> Jacob </p>
-                                    </td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>02/02/2002</td>
-                                    <td>asdf 620555</td>
-                                    <td className=' text-center'><i className="fa-solid fa-trash"></i></td>
-                                </tr>
->>>>>>> f31845cf5e8ebc3e9b49a1569cd69d6fa89f48fa
                                
                               
                             </tbody>
